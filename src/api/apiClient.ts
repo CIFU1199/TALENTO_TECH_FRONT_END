978a3853613957dbb09f105a2b900dff524f071c
CIFU1199 <edwkamilo@gmail.com>
2025-05-29 08:26:07 -0500

    Se quita la recarga de pantalla al  fallar las credenciales de acceso

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -23,7 +23,7 @@ apiClient.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('userData');
-      window.location.href = '/login';
+      //window.location.href = '/login';
     }
     return Promise.reject(error);
   }
